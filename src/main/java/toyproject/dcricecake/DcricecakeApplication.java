13055c6ee5101255078a1996e393b04f2211ddc7
seungbok3240 <seungbok3240@gmail.com>
2022-04-25 21:35:50 +0900

    create project

---
+++ src/main/java/toyproject/dcricecake/DcricecakeApplication.java
@@ -0,0 +1,13 @@
+package toyproject.dcricecake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcricecakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcricecakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyproject/dcricecake/DcricecakeApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.dcricecake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcricecakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8d5eb675b399dbb8e99fa690dcdac5fe4ce27fe8
seungbok3240 <seungbok3240@gmail.com>
2022-05-03 19:58:56 +0900

    customer sign up validation

--- src/main/java/toyproject/dcricecake/customer/domain/CustomerSignupForm.java
+++ src/main/java/toyproject/dcricecake/customer/domain/CustomerSignupForm.java
@@ -2,14 +2,19 @@ package toyproject.dcricecake.customer.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class CustomerSignupForm {
 
     private Long id;
 
+    @NotBlank
     private String loginId;
 
+    @NotBlank
     private String password;
 
+    @NotBlank
     private String checkPassword;
 }
